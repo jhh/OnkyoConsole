8d4685a35c9e0da7638de814e2ddece33a570d09
Jeff Hutchison <jeff@jeffhutchison.com>
2015-01-17 14:59:46 -0500

    Initial Commit

---
+++ Onkyo Console/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Onkyo Console
+//
+//  Created by Jeff Hutchison on 1/17/15.
+//  Copyright (c) 2015 Jeff Hutchison. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Onkyo Console/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Onkyo Console
+//
+//  Created by Jeff Hutchison on 1/17/15.
+//  Copyright (c) 2015 Jeff Hutchison. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
