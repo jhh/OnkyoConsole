d73cd1ca4f9a38390c3711f5fccf46eada92de73
Jeff Hutchison <jeff@jeffhutchison.com>
2015-01-17 15:39:41 -0500

    Initial check-in.

--- Onkyo Console/AppDelegate.h
+++ Onkyo Console/AppDelegate.h
@@ -2,14 +2,22 @@
 //  AppDelegate.h
 //  Onkyo Console
 //
-//  Created by Jeff Hutchison on 1/17/15.
-//  Copyright (c) 2015 Jeff Hutchison. All rights reserved.
+//  Created by Jeff Hutchison on 6/8/13.
+//  Copyright (c) 2013 Jeff Hutchison. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+@import Cocoa;
+#import "OnkyoKit.h"
+#import <os/activity.h>
+#import "ReceiverBrowser.h"
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, ReceiverBrowserDelegate, ONKReceiverDelegate>
 
+@property ReceiverBrowser *receiverBrowser;
+@property (unsafe_unretained) IBOutlet NSTextView *consoleTextView;
+@property (unsafe_unretained) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *commandTextField;
+
+- (IBAction)sendCommand:(NSTextField *)sender;
 
 @end
-
---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :osx, '10.10'
+
+pod 'OnkyoKit', :git => 'https://github.com/jhh/onkyokit.git', :branch => 'derp'
